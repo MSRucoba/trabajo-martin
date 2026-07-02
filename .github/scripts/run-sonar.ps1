9f760a5636d86bb8de3a99c129e52d76630fbe32
jhordyvela <velarojasjhordy@gmail.com>
2026-07-02 12:11:26 -0500

    Add Sonar900

---
+++ .github/scripts/run-sonar.ps1
@@ -0,0 +1,20 @@
+param(
+  [string]$SonarHostUrl,
+  [string]$SonarToken,
+  [string]$Workspace
+)
+
+$dockerArgs = @(
+  'run',
+  '--rm',
+  '-e', "SONAR_TOKEN=$SonarToken",
+  '-v', "$Workspace:/usr/src",
+  '-w', '/usr/src',
+  'sonarsource/sonar-scanner-cli:latest',
+  '-Dsonar.host.url=' + $SonarHostUrl,
+  '-Dproject.settings=sonar-project.properties',
+  '-Dsonar.token=' + $SonarToken
+)
+
+& docker @dockerArgs
+exit $LASTEXITCODE
